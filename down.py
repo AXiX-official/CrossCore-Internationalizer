a92eef2720b3633ef3cc47e55b67bac80e9b660e
AXiX-official <2879710747@qq.com>
2024-02-11 23:51:45 +0800

    更新custom包

---
+++ down.py
@@ -0,0 +1,21 @@
+import requests
+from tqdm import tqdm
+
+platform = "android"
+# platform = "ios"
+
+# 基础 URL
+base_url = f"https://cdn.megagamelog.com/cross/release/{platform}/curr_1/custom/"
+
+# 读取文件
+with open("ilist.txt", "r") as file:
+    items = file.read().split(',')
+
+# 下载文件
+for item in tqdm(items):
+    url = base_url + item
+    response = requests.get(url)
+    
+    # 将文件保存到本地
+    with open(f"custom/{item}", 'wb') as f:
+        f.write(response.content)
